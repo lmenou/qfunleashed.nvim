a0850bf62d35b21482d17426ecb24d0510ae5ca1
lmenou <menl94629@gmail.com>
2022-01-07 15:57:29 +0100

    Refactor buffer creation

--- lua/buffer.lua
+++ lua/buffer.lua
@@ -1,21 +1,36 @@
 local api, fn = vim.api, vim.fn
--- local M = {}
+local M = {}
 
-Create_buf = function()
-  api.nvim_command [[topleft 10split UnleashedStatus]]
+local create_buf = function()
+  api.nvim_command [[topleft 20split UnleashedStatus]]
+
   vim.bo.bufhidden = 'hide'
   vim.bo.buflisted = true
   vim.bo.swapfile = false
+
   vim.wo.wrap = false
+  vim.wo.winfixheight = true
+
   vim.api.nvim_command [[set filetype=unleashed]]
   return fn.getbufinfo(0).bufnr
 end
 
-Buf_set_lines = function()
-  local bufnr = Create_buf()
+local buffer_wipe = function(bufnr)
   api.nvim_buf_set_option(bufnr, 'modifiable', true)
   api.nvim_buf_set_option(bufnr, 'readonly', false)
   api.nvim_buf_set_lines(bufnr, 0, -1, false, {})
+end
+
+local buffer_nonmodif = function(bufnr)
+  api.nvim_buf_set_option(bufnr, 'modifiable', false)
+  api.nvim_buf_set_option(bufnr, 'readonly', true)
+  api.nvim_buf_set_option(bufnr, 'modified', false)
+  api.nvim_buf_set_option(bufnr, 'buflisted', false)
+end
+
+M.buf_set_lines = function()
+  local bufnr = create_buf()
+  buffer_wipe(bufnr)
 
   local lines = {
     "UnleashedStatus:",
@@ -29,11 +44,9 @@ Buf_set_lines = function()
   }
   api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
 
-  api.nvim_buf_set_option(bufnr, 'modifiable', false)
-  api.nvim_buf_set_option(bufnr, 'readonly', true)
-  api.nvim_buf_set_option(bufnr, 'modified', false)
+  buffer_nonmodif(bufnr)
 end
 
-Buf_set_lines()
+return M
 
--- return M
+-- lua: et tw=79 ts=2 sts=2 sw=2
--- syntax/unleashed.vim
+++ syntax/unleashed.vim
@@ -1,3 +1,7 @@
+if exists("b:current_syntax")
+  finish
+endif
+
 syntax match unHeader /^UnleashedStatus:$/
 syntax match quickHeader /^Quickfix\slist:$/
 syntax match locHeader /^Location\slist:$/
@@ -5,3 +9,7 @@ syntax match locHeader /^Location\slist:$/
 hi def link unHeader Function
 hi def link quickHeader Label
 hi def link locHeader Label
+
+let b:current_syntax = "unleashed"
+
+" vim: et ts=2 sts=2 sw=2 tw=79
