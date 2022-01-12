31381ac786a537bcc91e0d56212f555eaff323db
lmenou <menl94629@gmail.com>
2022-01-12 10:58:04 +0100

    Add buffer module

--- lua/buffer.lua
+++ lua/buffer.lua
@@ -1,52 +1,49 @@
 local api, fn = vim.api, vim.fn
-local M = {}
 
-local create_buf = function()
+-- To operate on the buffer
+local buf = {}
+
+function buf:create_buf()
   api.nvim_command [[topleft 20split UnleashedStatus]]
+  self.bufnr = fn.getbufinfo(0).bufnr
 
-  vim.bo.bufhidden = "hide"
-  vim.bo.buflisted = true
-  vim.bo.swapfile = false
+  api.nvim_buf_set_option(self.bufnr, "bufhidden", "hide")
+  api.nvim_buf_set_option(self.bufnr, "buflisted", true)
+  api.nvim_buf_set_option(self.bufnr, "swapfile", true)
 
-  vim.wo.wrap = false
-  vim.wo.winfixheight = true
+  api.nvim_win_set_option(0, "wrap", true)
+  api.nvim_win_set_option(0, "winfixheight", true)
 
   vim.api.nvim_command [[set filetype=unleashed]]
-  return fn.getbufinfo(0).bufnr
 end
 
-local buffer_wipe = function(bufnr)
-  api.nvim_buf_set_option(bufnr, "modifiable", true)
-  api.nvim_buf_set_option(bufnr, "readonly", false)
-  api.nvim_buf_set_lines(bufnr, 0, -1, false, {})
+function buf:buffer_wipe()
+  api.nvim_buf_set_option(self.bufnr, "modifiable", true)
+  api.nvim_buf_set_option(self.bufnr, "readonly", false)
+  api.nvim_buf_set_lines(self.bufnr, 0, -1, false, {})
 end
 
-local buffer_nonmodif = function(bufnr)
-  api.nvim_buf_set_option(bufnr, "modifiable", false)
-  api.nvim_buf_set_option(bufnr, "readonly", true)
-  api.nvim_buf_set_option(bufnr, "modified", false)
-  api.nvim_buf_set_option(bufnr, "buflisted", false)
+function buf:buffer_nonmodif()
+  api.nvim_buf_set_option(self.bufnr, "modifiable", false)
+  api.nvim_buf_set_option(self.bufnr, "readonly", true)
+  api.nvim_buf_set_option(self.bufnr, "modified", false)
+  api.nvim_buf_set_option(self.bufnr, "buflisted", false)
 end
 
-M.buf_set_lines = function()
-  local bufnr = create_buf()
-  buffer_wipe(bufnr)
+function buf:buf_set_lines(t)
+  if next(self) then
+    self:create_buf()
+  end
+  buf:buffer_wipe()
 
-  local lines = {
-    "UnleashedStatus:",
-    "  2 jobs are running",
-    "",
-    "Quickfix list:",
-    "==Details==",
-    "",
-    "Location list:",
-    "==Details==",
-  }
-  api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+  if t.set_lines then
+    local lines = t.set_lines
+    api.nvim_buf_set_lines(self.bufnr, 0, -1, false, lines)
+  end
 
-  buffer_nonmodif(bufnr)
+  self:buffer_nonmodif()
 end
 
-return M
+return buf
 
 -- lua: et tw=79 ts=2 sts=2 sw=2
